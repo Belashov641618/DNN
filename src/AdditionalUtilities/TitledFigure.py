cf332ade5ad8cde958db75e4b2ee92bdb8dd9a59
Belashov641618 <Uclaptrap@gmail.com>
2023-04-18 23:40:15 +0300

    Новые методы и новая структура кода

--- src/AdditionalUtilities/TitledFigure.py
+++ src/AdditionalUtilities/TitledFigure.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 
 class Titles:
-    def __init__(self, figure, titles, wspace=-1, hspace=-1, topspace=0.1, bottomspace=0.05, leftspace=0.05, rightspace=0.05):
+    def __init__(self, figure:plt.Figure, titles, wspace=-1, hspace=-1, topspace=0.05, bottomspace=0.01, leftspace=0.005, rightspace=0.005):
         self.figure = figure
 
         self.titles = titles
@@ -18,8 +19,10 @@ class Titles:
         self.leftspace = leftspace
         self.rightspace = rightspace
 
-        self.wpg = (1.0 - wspace * (titles[0] - 1) - leftspace - rightspace) / titles[0]
-        self.hpg = (1.0 - hspace * (titles[1] - 1) - topspace - bottomspace) / titles[1]
+        self._recalc()
+    def _recalc(self):
+        self.wpg = (1.0 - self.wspace * (self.titles[0] - 1) - self.leftspace - self.rightspace) / self.titles[0]
+        self.hpg = (1.0 - self.hspace * (self.titles[1] - 1) - self.topspace - self.bottomspace) / self.titles[1]
     def _repos(self, pos1, pos2):
         left    = (min(pos1[0], pos2[0]) - 1) * (self.wpg + self.wspace) + self.leftspace
         bottom  = 1.0 - (max(pos1[1], pos2[1]) - 1) * (self.hpg + self.hspace) - self.topspace - self.hpg
@@ -28,8 +31,44 @@ class Titles:
 
         return [left, bottom, width, height]
 
-    def add_axes(self, pos1, pos2=(-1,-1), projection=None):
+    def add_axes(self, pos1, pos2=(-1,-1), projection=None) -> Axes:
         if pos2 == (-1,-1):
             pos2 = pos1
         return self.figure.add_axes(self._repos(pos1, pos2), projection=projection)
 
+    def add_top_annotation(self, text:str, **text_kwargs):
+        self.topspace += 0.1
+        self._recalc()
+        self.figure.text(0.5, 0.95, text, text_kwargs)
+    def add_bottom_annotation(self, text:str, **text_kwargs):
+        text_kwargs.update({'verticalalignment':'top'})
+        self.bottomspace += 0.1
+        self._recalc()
+        self.figure.text(0.5, 0.05, text, text_kwargs)
+
+    _ColumnAnnotationEnable = False
+    def add_column_annotation(self, col_from:int, col_to:int=None, text:str='',  **text_kwargs):
+        text_kwargs.update({'rotation': 0})
+
+        if not self._ColumnAnnotationEnable:
+            self.bottomspace += 0.1
+            self._recalc()
+            self._ColumnAnnotationEnable = True
+        if col_to is None: col_to = col_from
+
+        left, bottom, width, height = self._repos((col_from,0), (col_to,0))
+        self.figure.text(left + width/2, 0.05, text, text_kwargs)
+
+    _RowAnnotationEnable = False
+    def add_row_annotation(self, row_from:int, row_to:int=None, text:str='',  **text_kwargs):
+        text_kwargs.update({'rotation': 90})
+        text_kwargs.update({'horizontalalignment':'right'})
+
+        if not self._RowAnnotationEnable:
+            self.leftspace += 0.05
+            self._recalc()
+            self._RowAnnotationEnable = True
+        if row_to is None: row_to = row_from
+
+        left, bottom, width, height = self._repos((0, row_from), (0, row_to))
+        self.figure.text(0.025, bottom+height/2, text, text_kwargs)
---
+++ src/Belashov/Layers/DetectorsLayer.py
@@ -0,0 +1,118 @@
+import torch
+from torchvision.transforms.functional import resize as f_resize
+from torchvision.transforms import InterpolationMode
+from typing import Union, Iterable
+from copy import deepcopy
+from src.AdditionalUtilities.DecimalPrefixes import mm, um, nm
+
+from src.Belashov.Layers.AbstractLayer import AbstractLayer
+
+
+class DetectorsLayer(AbstractLayer):
+    @staticmethod
+    def GenerateSquareDetectorsMasks(plane_length:float, total_pixels_count:int, detector_length:float=None, detectors_layout_border_ratio:float=0.9):
+        if detector_length is None:
+            detector_length = plane_length * (1.0-detectors_layout_border_ratio) / 5
+        if detector_length*4 > plane_length*(1.0-detectors_layout_border_ratio):
+            raise ValueError('Detector length is to large')
+
+        initial_detector_centers = torch.tensor([
+            [-detector_length,      +detector_length],
+            [0,                     +detector_length],
+            [+detector_length,      +detector_length],
+
+            [-1.5*detector_length,  0],
+            [-0.5*detector_length,  0],
+            [+0.5*detector_length,  0],
+            [+1.5*detector_length,  0],
+
+            [-detector_length,      -detector_length],
+            [0,                     -detector_length],
+            [+detector_length,      -detector_length]
+        ], dtype=torch.float64)
+        initial_detector_centers = initial_detector_centers * (plane_length * (1.0-detectors_layout_border_ratio) / (3.0*detector_length) - 1.0/3.0)
+
+        detectors_rectangles = initial_detector_centers.unsqueeze(dim=2).repeat(1,1,2).swapdims(1,2)
+        detectors_rectangles[:, 0, :] = detectors_rectangles[:, 0, :] - 0.5*detector_length
+        detectors_rectangles[:, 1, :] = detectors_rectangles[:, 1, :] + 0.5*detector_length
+        detectors_rectangles = detectors_rectangles + plane_length / 2
+
+        detectors_rectangles_pixels = (detectors_rectangles * total_pixels_count / plane_length).to(torch.int32)
+        detectors_rectangles_pixels = torch.where(detectors_rectangles_pixels >= total_pixels_count, total_pixels_count-1, detectors_rectangles_pixels)
+        detectors_rectangles_pixels = torch.where(detectors_rectangles_pixels < 0, 0, detectors_rectangles_pixels)
+
+        DetectorsMasks = torch.zeros((len(initial_detector_centers), total_pixels_count, total_pixels_count), dtype=torch.float64)
+        for num, rectangle in enumerate(detectors_rectangles_pixels):
+            DetectorsMasks[num, rectangle[0][0]:rectangle[1][0]+1, rectangle[0][1]:rectangle[1][1]+1] = torch.ones(rectangle[1][0]-rectangle[0][0]+1, rectangle[1][1]-rectangle[0][1]+1)
+
+        return DetectorsMasks
+
+    @staticmethod
+    def GenerateDetectorsMasksStringRedirector(generating_type:str, plane_length:float, total_pixels_count:int):
+        if generating_type == 'Square':
+            return DetectorsLayer.GenerateSquareDetectorsMasks(plane_length, total_pixels_count)
+        else:
+            raise ValueError("\033[31m\033[1m{}".format('GenerateDetectorsMasksStringRedirector: there is no generating_type with name ' + generating_type + '!'))
+
+
+    _DetectorsMasksBuffer : torch.Tensor
+    def _recalc_DetectorsMasksBuffer(self):
+        if self._DetectorsMasksBuffer.size() != torch.Size((10, self._PixelsCount*self._UpScaling, self._PixelsCount*self._UpScaling)):
+            DetectorsMaskCopy = self._DetectorsMasksBuffer.clone().detach()
+            self.register_buffer('_DetectorsMasksBuffer', f_resize(DetectorsMaskCopy.expand(1,-1,-1,-1), [self._PixelsCount*self._UpScaling, self._PixelsCount*self._UpScaling], interpolation=InterpolationMode.NEAREST).squeeze())
+    def GetDetectorsMasksBuffer(self, to_cpu:bool=True):
+        if to_cpu:
+            return deepcopy(self._DetectorsMasksBuffer).cpu()
+        return deepcopy(self._DetectorsMasksBuffer)
+    def GetDetectorsMasksBufferSum(self, to_cpu:bool=True):
+        if to_cpu:
+            return torch.sum(deepcopy(self._DetectorsMasksBuffer)).cpu()
+        return torch.sum(deepcopy(self._DetectorsMasksBuffer))
+
+
+    _PixelsCount : int
+    @property
+    def PixelsCount(self):
+        return self._PixelsCount
+    @PixelsCount.setter
+    def PixelsCount(self, pixels_count:int):
+        self._PixelsCount = pixels_count
+        self._add_DelayedFunction(self._recalc_DetectorsMasksBuffer, 1.0)
+
+    _UpScaling : int
+    @property
+    def UpScaling(self):
+        return self._UpScaling
+    @UpScaling.setter
+    def UpScaling(self, up_scaling:int):
+        self._UpScaling = up_scaling
+        self._add_DelayedFunction(self._recalc_DetectorsMasksBuffer, 1.0)
+
+
+    def __init__(self, pixels_count:int=50, up_scaling:int=20, detectors_masks:Union[str,Iterable,torch.Tensor]='Square'):
+        super(DetectorsLayer, self).__init__()
+
+        self._PixelsCount   = pixels_count
+        self._UpScaling     = up_scaling
+
+        if isinstance(detectors_masks, str):
+            detectors_masks = DetectorsLayer.GenerateDetectorsMasksStringRedirector(detectors_masks, 1.0, pixels_count*up_scaling)
+        elif not torch.is_tensor(detectors_masks):
+            detectors_masks = torch.tensor(detectors_masks)
+        detectors_masks.to(self._tensor_float_type)
+
+        self.register_buffer('_DetectorsMasksBuffer', detectors_masks)
+
+    def forward(self, field:torch.Tensor):
+        super(DetectorsLayer, self).forward(field)
+        results = torch.sum((torch.abs(field)**2).expand(10,-1,-1,-1,-1).movedim(0,2) * self._DetectorsMasksBuffer, dim=(1,3,4))
+
+        field_integral = torch.sum(torch.abs(field)**2, dim=(1, 2, 3))
+        results = results / field_integral.expand(10, -1).swapdims(0,1)
+
+        # max_results = torch.mean(results, dim=1)
+        # results = results / max_results.expand(10,-1).swapdims(0,1)
+
+        # results = torch.softmax(results, dim=1)
+
+        return results
